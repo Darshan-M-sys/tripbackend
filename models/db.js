caca0af3458e74775f6c38c628a5a24d0d6cc393
Darshan-M-sys <darshandarshan94356@gmail.com>
2025-09-27 21:50:40 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 const connectDB=()=>{
-  mongoose.connect(process.env.MONGO_URL)
+  mongoose.connect(process.env.MoNGO_URL)
   .then(()=>console.log("Database is connected"))
   .catch(()=>console.log("something error in the database connection"))
 }
